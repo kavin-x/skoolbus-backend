3fce0bc6692e89272a26bbea2d8dd9b61a56596f
kavin-x <kavinkumarab@gamil.com>
2022-12-28 10:32:30 +0530

    feat: configured to push in heroku

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 # import dj_database_url
+import django_heroku
 from pathlib import Path
 import os
 
@@ -132,3 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
